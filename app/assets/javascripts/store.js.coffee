26cb5eea897847eacf27d5f5c7dbe5a7fc082e18
Billy Shih <me@billyshih.com>
2013-06-12 23:17:52 -0700

    Working except for add winner with strong parameters

--- app/assets/javascripts/store.js.coffee
+++ app/assets/javascripts/store.js.coffee
@@ -1,4 +1,10 @@
 Raffler.Store = DS.Store.extend
-  revision: 4
-  adapter: DS.RESTAdapter.create()
+  revision: 11
 
+DS.RESTAdapter.configure("plurals", entry: "entries")
+
+# DS.RESTAdapter.configure("plurals", { entry: "entries" });
+# Raffler.Store = DS.Store.extend({
+#   revision: 11,
+#   adapter: DS.RESTAdapter.create()
+# });
---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,34 @@
+class EntriesController < ApplicationController
+  before_filter :find_entry, only: [:update]
+  respond_to :json
+
+  def index
+    respond_with Entry.all
+  end
+
+  def show
+    respond_with Entry.find(params[:id])
+  end
+
+  def create
+    # respond_with Entry.create(params[:entry])
+    respond_with Entry.create(entry_params)
+  end
+
+  def update
+    respond_with Entry.update(params[:id], params[:entry])
+  end
+
+  def destroy
+    respond_with Entry.destroy(params[:id])
+  end
+
+  private 
+  def entry_params
+    params.require(:entry).permit(:name, :winner)
+  end
+
+  def find_entry
+    @entry = Entry.find(params[:id])
+  end
+end
